6a4e6dfbea55fa3e12da63326a62ea2eb4385d91
jssgsy <fcsnwu@163.com>
2016-01-20 15:01:40 +0800

    新增spring的Resource测试

--- spring/src/main/java/com/univ/junit/JUnitTest.java
+++ spring/src/main/java/com/univ/junit/JUnitTest.java
@@ -1,9 +1,13 @@
 package com.univ.junit;
 
+import java.io.IOException;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 import com.univ.ioc.Student;
@@ -48,5 +52,43 @@ public class JUnitTest {
          String key3 = resources.getMessage("key3", null, "这是没有找到key3所返回的值", null);
          System.out.println("key3: " + key3); 
     }
+    
+    /**
+     * 测试spring的Resource
+     * Resource有很多种实现,其实例创建一般有两种方法，一种用构造函数，一种如下所示：
+	 * 	   Resource resource = ac.getResource("classpath:applicationContext.xml");
+	 *     System.out.println(resource.getDescription());
+	 *     //输出：class path resource [applicationContext.xml]
+	 *     此时获得的Resource实例类型由调用的application context决定。
+	 * Resource可以直接被注入给bean，需要注意的是注入的是一个表示资源的String
+     */
+    @Test
+    public void resource(){
+    	/**
+    	 * ClassPathResource,即使log4j.properties不存在也不会报错，只是此时exists等方法返回false而已,所以使用之前先判断是否存在
+    	 */
+    	//如果类路径下有test文件夹，其下有blaba.txt，则读取方法如下；
+    	//ClassPathResource resource1 = new ClassPathResource("test/blaba.txt");
+    	
+    	ClassPathResource classpath = new ClassPathResource("log4j.properties");    
+    	System.out.println(classpath.getFilename());
+    	System.out.println(classpath.getDescription());
+    	System.out.println(classpath.exists());
+    	System.out.println(classpath.isOpen());
+    	System.out.println(classpath.isReadable());
+    	System.out.println("---------------------------------------------");
+    	/**
+    	 * FileSystemResource,即使test.txt不存在也不会报错，只是此时exists等方法返回false而已,所以使用之前先判断是否存在
+    	 */
+    	FileSystemResource fileSys = new FileSystemResource("c:\\test.txt");
+    	System.out.println(fileSys.getFilename());
+    	System.out.println(fileSys.getFilename());
+    	System.out.println(fileSys.getDescription());
+    	System.out.println(fileSys.exists());
+    	System.out.println(fileSys.isOpen());
+    	System.out.println(fileSys.isReadable());
+    	
+    }
+    
 }
 
