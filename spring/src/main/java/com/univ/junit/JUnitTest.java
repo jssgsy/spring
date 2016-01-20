ff4cfd6706c23f5ff142c6b955ad22e715f2b9b0
jssgsy <fcsnwu@163.com>
2016-01-20 12:00:09 +0800

    完善MessageSource功能的使用

--- spring/src/main/java/com/univ/junit/JUnitTest.java
+++ spring/src/main/java/com/univ/junit/JUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 
 import com.univ.ioc.Student;
 
@@ -26,12 +27,26 @@ public class JUnitTest {
         System.out.println(student.getTeacher().getName());
     }
     
+    /**
+     * 测试spring的MessageSource用法
+     * 更常用的方法是将MessageSource注入到需要使用的bean中，例子见参考手册；
+     * 关于MessageSource更多参考配置文件中bean的定义；
+     * key的搜索顺序是配置文件中定义的顺序，一找到则返回；
+     */
     @Test
     public void  resourceBundleMessageSourceTest(){
     	 MessageSource resources = new ClassPathXmlApplicationContext("applicationContext.xml");
-    	//到messages.properties（见配置文件） 找key等于aaa的值，找到则返回，找不到则返回这里的“Default”字符串
-         String message = resources.getMessage("aaa", null, "default string to return if not found aaa", null);
-         System.out.println(message); 
+    	//到messages.properties（见配置文件） 找key等于key1的值
+    	 String key1 = resources.getMessage("key1", null,"no default value for key1",null);
+    	 System.out.println("key1: " + key1);
+    	 
+    	 //传参，如果配置文件中的形参多余这里的实参，则形参将被解析成字符串
+         String key2 = resources.getMessage("key2", new String[]{"liu","univ"}, "default string to return if not found aaa", null);
+         System.out.println("key2: " + key2); 
+         
+         //找不到则返回这里的默认字符串
+         String key3 = resources.getMessage("key3", null, "这是没有找到key3所返回的值", null);
+         System.out.println("key3: " + key3); 
     }
 }
 
