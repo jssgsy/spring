2b8fe3a0b0c4b45f6a7e272fa5f24a74b247d67a
jssgsy <fcsnwu@163.com>
2016-01-21 09:44:14 +0800

    新增Properties类型的注入

---
+++ spring/src/main/java/com/univ/ioc/MyProperties.java
@@ -0,0 +1,37 @@
+package com.univ.ioc;
+
+import java.util.Properties;
+
+/** 
+ * @author univ 
+ * @date 2016年1月21日 上午9:16:14 
+ * @version v1.0
+ * @Description: 
+ */
+public class MyProperties {
+
+	private String name;
+	private Properties pros1;
+	private Properties pros2;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Properties getPros1() {
+		return pros1;
+	}
+	public void setPros1(Properties pros1) {
+		this.pros1 = pros1;
+	}
+	public Properties getPros2() {
+		return pros2;
+	}
+	public void setPros2(Properties pros2) {
+		this.pros2 = pros2;
+	}
+	
+	
+}
+
